7640e35e4398aa43f70cba8852483085c58eb7f5
Jamie Tumalty <jamietumalty@gmail.com>
2022-04-10 22:46:29 +0100

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,39 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+const path = require("path")
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-root-import",
+  ],
+  siteMetadata: {
+    title: "Jamie Tumalty",
+    social: [
+      {
+        name: "Twitter",
+        href: "",
+        icon: "Twitter",
+      },
+      {
+        name: "Github",
+        href: "",
+        icon: "Github",
+      },
+      {
+        name: "LinkedIn",
+        href: "",
+        icon: "LinkedIn",
+      },
+      //{
+      //  name: "ItchIo",
+      //  href: "",
+      //  icon: "ItchIo",
+      //},
+    ],
+  },
 }
