165e3d6c862a4e8302498ac89a5e7ea352565ba1
Jamie Tumalty <jamietumalty@gmail.com>
2023-05-18 14:37:30 +0100

    Typescript config

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const Test = () => {
+  return <div>Hello World!</div>;
+};
+
+export default function Home() {
+  return (
+    <>
+      <Test />
+      <Test />
+    </>
+  );
+}
